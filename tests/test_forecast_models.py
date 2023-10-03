0bf89b0dfbb6b2d325dad363ca3dec9ddbf83cff
VincentPro <145966298+GitHubVincentPro@users.noreply.github.com>
2023-10-03 18:38:51 +0200

    tests

--- tests/test_forecast_models.py
+++ tests/test_forecast_models.py
@@ -1,20 +1,45 @@
 # test_forecast_models.py
-import forecast_models
+import pytest
 
-from unittest import TestCase
+from models import forecasting
 
-class TestNaiveForecast(TestCase):
+@pytest.fixture
+def solar_data():
+# utiliser la fixture defined dans conftest
+return pytest.solar_data
 
-def test_predict(self):
-# entraîner le modèle
-# générer des prédictions
-# vérifier qu'elles sont proches des vraies données
+@pytest.fixture
+def rnn_model():
+return forecasting.RNNForecaster()
 
-class TestRNNForecast(TestCase):
+class TestRNNForecasting:
 
-def test_train(self):
-# entraîner le RNN
-# vérifier que la loss diminue
+def test_train(self, solar_data, rnn_model):
 
-def test_predict(self):
-# tester les prédictions
+# Entraîner le modèle
+rnn_model.train(solar_data)
+
+# Vérifier que la loss décroît
+losses = rnn_model.history['loss']
+assert losses[-1] < losses[0]
+
+def test_predict(self, solar_data, rnn_model):
+
+# Prédire sur des données de test
+preds = rnn_model.predict(solar_data['test'])
+
+# Vérifier la qualité des prédictions
+score = mean_absolute_error(preds, solar_data['test']['target'])
+assert score < 0.1
+
+class TestNaiveForecasting:
+
+def test_predict(self, solar_data):
+
+# Instancier le modèle basique
+model = forecasting.NaiveForecaster()
+
+# Prédire et vérifier la qualité
+preds = model.predict(solar_data['test'])
+score = mean_absolute_error(preds, solar_data['test']['target'])
+assert score < 0.2
