c7f7d0fa684953fecfba39839c78081dddd8997f
VincentPro <145966298+GitHubVincentPro@users.noreply.github.com>
2023-10-03 20:41:47 +0200

    run

--- run.py
+++ run.py
@@ -1,8 +1,29 @@
 # run.py
 
-from app import create_app
+import os
+from flask import Flask
+from app import config
+from app.api import api_blueprint
+from app.models import prediction_model, storage_model
 
-app = create_app('development')
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['DEBUG'] = True
+
+app.register_blueprint(api_blueprint)
+
+@app.route('/')
+def home():
+return 'Welcome to the EnergyHub API!'
+
+@app.route('/status')
+def status():
+return {'status': 'operational'}
 
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=5000, debug=True)
+
+prediction_model.load()
+storage_model.load()
+
+app.run(host='0.0.0.0', port=5000)
