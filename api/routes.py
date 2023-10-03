0bad5040765009e175e97232be14809ef8b71b2f
VincentPro <145966298+GitHubVincentPro@users.noreply.github.com>
2023-10-03 20:03:15 +0200

    api

--- api/routes.py
+++ api/routes.py
@@ -1,4 +1,4 @@
-```python
+python
 from flask import Blueprint, request, jsonify
 from core import predict, get_storage
 
@@ -13,4 +13,42 @@ return jsonify(forecast)
 def storage():
 status = get_storage()
 return jsonify(status)
-```
+
+
+python
+@api.route('/forecast')
+def forecast():
+
+try:
+forecast = predict(request.json['data'])
+return jsonify(forecast)
+
+except Exception as e:
+return jsonify({"error": str(e)}), 400
+
+python
+from schemas import ForecastSchema
+
+@api.route('/forecast')
+def forecast():
+
+data = request.json
+
+try:
+ForecastSchema().load(data)
+
+except ValidationError as e:
+return jsonify(e.messages), 422
+
+forecast = predict(data)
+return jsonify(forecast)
+
+python
+@api.route('/simulation', methods=['POST'])
+def simulation():
+
+scenario = request.json
+
+results = run_simulation(scenario)
+
+return jsonify(results)
