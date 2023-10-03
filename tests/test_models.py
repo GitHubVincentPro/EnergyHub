3ffa85a11e99e982e8146654ddf896a4d58e416a
VincentPro <145966298+GitHubVincentPro@users.noreply.github.com>
2023-10-03 20:59:13 +0200

    tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,43 +1,14 @@
-import pytest
-from models import forecasting
+# tests/test_models.py
 
-@pytest.fixture
-def solar_data():
-# Charge les données solaires
-return ...
+import unittest
+from models import forecast_model
 
-@pytest.fixture
-def rnn_model():
-return forecasting.RNNForecaster()
+class TestForecasting(unittest.TestCase):
 
-class TestRNNForecasting:
+def test_forecast_single_point(self):
+forecast = forecast_model.predict(1)
+self.assertEqual(forecast[0], 100) # Prévision attendue
 
-def test_train(self, solar_data, rnn_model):
-
-# Entraîne le modèle
-rnn_model.train(solar_data)
-
-# Valide que la loss diminue
-losses = rnn_model.history['loss']
-assert losses[-1] < losses[0]
-
-def test_predict(self, solar_data, rnn_model):
-
-# Prédit sur données de test
-preds = rnn_model.predict(solar_data['test'])
-
-# Vérifie qualité des prédictions
-score = mean_absolute_error(preds, solar_data['test']['target'])
-assert score < 0.1
-
-class TestNaiveForecasting:
-
-def test_predict(self, solar_data):
-
-# Instancie modèle basique
-model = forecasting.NaiveForecaster()
-
-# Teste prédictions
-preds = model.predict(solar_data['test'])
-score = mean_absolute_error(preds, ...)
-assert score < 0.2
+def test_forecast_multiple_points(self):
+forecasts = forecast_model.predict(5)
+self.assertEqual(len(forecasts), 5)
