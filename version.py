2530ae6b00b076e49928b827c7f4a79db6edfd67
VincentPro <145966298+GitHubVincentPro@users.noreply.github.com>
2023-10-03 17:16:10 +0200

    version

--- version.py
+++ version.py
@@ -1,1 +1,14 @@
-Définit le numéro de version du package
+##Définit le numéro de version du package
+
+```python
+__title__ = 'energyhub'
+__description__ = 'EnergyHub optimises renewable energy production, storage and distribution using AI.'
+__url__ = 'https://github.com/GitHubVincentPro/EnergyHub'
+__version__ = '0.1.0'
+__author__ = 'Vincent Pro'
+__author_email__ = 'vincent.pro@energyhub.com'
+__license__ = 'MIT'
+
+def get_version():
+return __version__
+```
