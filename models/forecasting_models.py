2bf73156805f90e181acbf9f1e9503f8211566ca
VincentPro <145966298+GitHubVincentPro@users.noreply.github.com>
2023-10-03 19:40:52 +0200

    models

--- models/forecasting_models.py
+++ models/forecasting_models.py
@@ -1,23 +1,36 @@
 ```python
-# models/forecasting_models.py
+# Import des bibliothèques Keras et Tensorflow
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+def lstm_model(input_dim, output_dim):
 
-class LSTMModel:
+model = Sequential()
+model.add(LSTM(100, input_shape=(input_dim,1)))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(output_dim))
 
-def __init__(self):
-self.model = Sequential()
-self.model.add(LSTM(50, input_shape=(lookback, 1)))
-self.model.add(Dense(1))
+model.compile(loss='mse', optimizer='adam')
+return model
 
-def fit(self, x_train, y_train):
-self.model.compile(optimizer='adam', loss='mse')
-self.model.fit(x_train, y_train, epochs=100)
+def train_lstm(X_train, y_train, epochs=100):
 
-def predict(self, x_test):
-return self.model.predict(x_test)
+model = lstm_model(X_train.shape[1], y_train.shape[1])
 
+model.fit(X_train, y_train, epochs=epochs)
+
+return model
 ```
 
-# Notebook d'entraînement du LSTM
+### Dans notebooks/train_rnn_model.ipynb
+
+```python
+from models.forecasting_models import train_lstm
+
+# Entraînement du modèle LSTM sur les données solaires
+lstm_model = train_lstm(X_solar, y_solar)
+
+# Évaluation sur le jeu de test
+lstm_model.evaluate(X_test, y_test)
+```
