1cd3f731733588f3cdca59e6116a2f480ba72bd5
VincentPro <145966298+GitHubVincentPro@users.noreply.github.com>
2023-10-03 19:55:02 +0200

    app

---
+++ ui/app/Simulation.vue
@@ -0,0 +1,48 @@
+html
+<template>
+<div>
+
+<!-- Configuration du scénario -->
+<ScenarioConfig
+v-model="scenario"
+/>
+
+<!-- Affichage des résultats -->
+<SimulationResults
+v-if="results"
+:results="results"
+/>
+
+<button
+@click="startSimulation"
+>
+Lancer
+</button>
+
+</div>
+</template>
+
+<script>
+import ScenarioConfig from './ScenarioConfig.vue'
+import SimulationResults from './SimulationResults.vue'
+
+export default {
+components: {
+ScenarioConfig,
+SimulationResults
+},
+
+data() {
+return {
+scenario: {},
+results: null
+}
+},
+
+methods: {
+async startSimulation() {
+// Appeler le service de simulation
+}
+}
+}
+</script>
