c11090fc9b726bf9a1f3a125d1c4c679eae40b3c
VincentPro <145966298+GitHubVincentPro@users.noreply.github.com>
2023-10-03 20:06:35 +0200

    tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,11 @@
-```python
+python
 import core
 
 def test_forecast():
 # Test prévisions énergie
-```
+
+python
+from api.core import predict
+
+def test_predict():
+# tests unitaires
---
+++ tests/test_routes.py
@@ -0,0 +1,5 @@
+python
+from api.app import app
+
+def test_forecast_endpoint():
+# tests d'intégration
