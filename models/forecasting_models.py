11fdaf88563e71ddc1f668186ba1a66b1be25403
VincentPro <145966298+GitHubVincentPro@users.noreply.github.com>
2023-10-03 20:52:32 +0200

    models

--- models/forecasting_models.py
+++ models/forecasting_models.py
@@ -1,36 +1,13 @@
-```python
-# Import des bibliothèques Keras et Tensorflow
-from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
-def lstm_model(input_dim, output_dim):
+def train_linear_regression(X, y):
 
-model = Sequential()
-model.add(LSTM(100, input_shape=(input_dim,1)))
-model.add(Dense(50, activation='relu'))
-model.add(Dense(output_dim))
+lr = LinearRegression()
+lr.fit(X, y)
+return lr
 
-model.compile(loss='mse', optimizer='adam')
-return model
+def forecast_consumption(lr_model, new_data):
 
-def train_lstm(X_train, y_train, epochs=100):
-
-model = lstm_model(X_train.shape[1], y_train.shape[1])
-
-model.fit(X_train, y_train, epochs=epochs)
-
-return model
-```
-
-### Dans notebooks/train_rnn_model.ipynb
-
-```python
-from models.forecasting_models import train_lstm
-
-# Entraînement du modèle LSTM sur les données solaires
-lstm_model = train_lstm(X_solar, y_solar)
-
-# Évaluation sur le jeu de test
-lstm_model.evaluate(X_test, y_test)
-```
+X_new = new_data[['weather', 'hour']]
+return lr_model.predict(X_new)
