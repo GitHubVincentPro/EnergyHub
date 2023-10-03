88db0bea875f450b267d30f42beb2b639b91d263
VincentPro <145966298+GitHubVincentPro@users.noreply.github.com>
2023-10-03 20:09:11 +0200

    tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,4 @@
-python
+```python
 import unittest
 from api.core import predict
 from api.models import ForecastRequest, ForecastResult
@@ -7,32 +7,31 @@ class TestCore(unittest.TestCase):
 
 def setUp(self):
 self.request = ForecastRequest(
-date='2022-01-01',
-location='Paris'
+date="2022-01-01",
+location="Paris"
 )
 
 def test_predict_returns_result(self):
-"""La fonction predict doit retourner un objet ForecastResult"""
-
 result = predict(self.request)
-
 self.assertIsInstance(result, ForecastResult)
 
 def test_forecast_temperature(self):
-"""Le champ temperature du résultat doit être un float"""
-
 result = predict(self.request)
-
 self.assertIsInstance(result.temperature, float)
 
-def test_valid_forecast(self):
-"""La prévision météo doit être une chaine valide"""
-
+def test_valid_weather(self):
+possible_weather = ['sunny', 'rainy', 'cloudy']
 result = predict(self.request)
+self.assertIn(result.weather, possible_weather)
 
-valid_forecasts = ['soleil', 'pluie', 'nuageux']
+def test_empty_request(self):
+request = ForecastRequest()
+self.assertRaises(Exception, predict, request)
 
-self.assertIn(result.weather, valid_forecasts)
+def test_invalid_location(self):
+request = ForecastRequest(location="Invalid")
+self.assertRaises(Exception, predict, request)
 
 if __name__ == '__main__':
 unittest.main()
+```
