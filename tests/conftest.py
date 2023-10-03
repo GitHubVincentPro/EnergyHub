8813ed0efc0e39aba76ef8ec4c9d18adeafe4853
VincentPro <145966298+GitHubVincentPro@users.noreply.github.com>
2023-10-03 18:36:15 +0200

    tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,39 @@
 # conftest.py
 
 import pytest
+import pandas as pd
 
-# fixtures communes à tous les tests
+# Fixtures de données communes
+
+@pytest.fixture
+def solar_data():
+return pd.read_csv("../data/solar_production.csv")
+
+@pytest.fixture
+def load_data():
+return pd.read_csv("../data/load.csv")
+
+@pytest.fixture
+def prices_data():
+return pd.read_csv("../data/prices.csv")
+
+# Fixture de configuration
+
+@pytest.fixture
+def config():
+return {
+"api_url": "http://localhost:5000",
+"data_path": "../data/"
+}
+
+# Autres fixtures utiles
+
+@pytest.fixture
+def client():
+# Instancier un client d'API
+return ApiClient()
+
+@pytest.fixture
+def forecast_model():
+# Instancier un modèle pré-entraîné
+return RNNForecast()
